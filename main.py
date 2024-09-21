d2372b03d91f91eeed80dbe0ec0fe73f4b2352a8
nico <nicolas.ozz.com@gmail.com>
2024-09-21 18:47:09 +0200

    Set start command

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+import logging
 import os
+
 from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import *
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.info('Starting Bot...')
 
 load_dotenv()
 
-os.getenv("API_KEY")
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(os.getenv("API_KEY")).build()
+
+    application.add_handler(CommandHandler('start', start_command))
+
+    application.run_polling()
